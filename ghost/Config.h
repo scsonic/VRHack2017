8023966c7d2f2ef95f2380f488461042124c47a7
scsonic <yougangkuo@Youde-MacBook-Pro.local>
2017-08-25 20:55:59 +0800

    First commit
    First commit

---
+++ ghost/Config.h
@@ -0,0 +1,21 @@
+/*
+ * This is Config file for Thunder Tiger RoboHero
+ * 
+ * 
+ */
+
+
+/* using AP mode or not */
+#define AP_MODE 0
+
+/* for host robohero as wifi-ap mode */
+#define AP_PASSOWRD "12345678"
+
+/* use for mdns, you can change it for multipal robohero */
+#define MDNS_NAME "robohero"
+
+/* for connect your home wifi-ap */
+
+#define CLIENT_SSID "You Gang 的 iPhone"
+#define CLIENT_PASSWORD "0912078859"
+
